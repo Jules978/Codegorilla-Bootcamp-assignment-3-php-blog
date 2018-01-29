332362f32a4ff056a5a538dd652771aab5926229
JJD <vanDrunen.julia@gmail.com>
2018-01-29 17:15:09 +0100

    update1: base html and css

---
+++ script.js
@@ -0,0 +1,7 @@
+//1: submit new blogposts to database: Title, description, catagory, text, databbase adds ID (via auto increment)
+
+//2: get blogposts from database, display as a card div.
+
+//3: sort by newest, add catagories.
+
+//4. add delete and maybe edit option.
